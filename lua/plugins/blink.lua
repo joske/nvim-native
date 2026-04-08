41ac114beea418b9bb4a1920b895b6af0c534b4f
Jos Dehaes <jos.dehaes@gmail.com>
2026-04-08 16:30:42 +0200

    try to fix auto-build of blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -14,6 +14,20 @@ vim.pack.add(
     { confirm = false }
 )
 
+-- Build fuzzy matching library if missing
+local blink_path = vim.fn.stdpath("data") .. "/site/pack/core/opt/blink.cmp"
+if vim.fn.isdirectory(blink_path) == 1 then
+    local so_ext = vim.uv.os_uname().sysname == "Darwin" and "dylib" or "so"
+    local lib_path = blink_path .. "/target/release/libblink_cmp_fuzzy." .. so_ext
+    if vim.fn.filereadable(lib_path) == 0 then
+        vim.notify("blink.cmp: building fuzzy library...", vim.log.levels.INFO)
+        local result = vim.system({ "cargo", "build", "--release" }, { cwd = blink_path }):wait()
+        if result.code ~= 0 then
+            vim.notify("blink.cmp: build failed!\n" .. (result.stderr or ""), vim.log.levels.ERROR)
+        end
+    end
+end
+
 require("blink.cmp").setup({
     keymap = { preset = "enter" },
     signature = { enabled = true },
