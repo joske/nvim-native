72f672c890fd2c8a05d01ee984ee2870991ede33
Jos Dehaes <jos.dehaes@gmail.com>
2026-04-09 09:54:50 +0200

    use downloaded blink version

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,32 +1,13 @@
-vim.api.nvim_create_autocmd("PackChanged", {
-    callback = function(ev)
-        if ev.data.spec.name == "blink.cmp" and (ev.data.kind == "install" or ev.data.kind == "update") then
-            vim.system({ "cargo", "build", "--release" }, { cwd = ev.data.spec.path }):wait()
-        end
-    end,
-})
-
 vim.pack.add(
     {
-        { src = "https://github.com/saghen/blink.cmp" },
+        {
+            src = "https://github.com/saghen/blink.cmp",
+            version = vim.version.range('1')
+        },
         { src = "https://github.com/rafamadriz/friendly-snippets" },
     },
     { confirm = false }
 )
-
--- Build fuzzy matching library if missing
-local blink_path = vim.fn.stdpath("data") .. "/site/pack/core/opt/blink.cmp"
-if vim.fn.isdirectory(blink_path) == 1 then
-    local so_ext = vim.uv.os_uname().sysname == "Darwin" and "dylib" or "so"
-    local lib_path = blink_path .. "/target/release/libblink_cmp_fuzzy." .. so_ext
-    if vim.fn.filereadable(lib_path) == 0 then
-        vim.notify("blink.cmp: building fuzzy library...", vim.log.levels.INFO)
-        local result = vim.system({ "cargo", "build", "--release" }, { cwd = blink_path }):wait()
-        if result.code ~= 0 then
-            vim.notify("blink.cmp: build failed!\n" .. (result.stderr or ""), vim.log.levels.ERROR)
-        end
-    end
-end
 
 require("blink.cmp").setup({
     keymap = { preset = "enter" },
