5d4e5cd624e25d7ff1efd51e744d1c3c1b2a55f7
Jos Dehaes <jos.dehaes@gmail.com>
2026-08-13 12:19:23 +0200

    make gq work again

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,1 +1,12 @@
 vim.treesitter.start()
+
+-- keep gq/gw on the internal paragraph formatter: any attached LSP advertising
+-- rangeFormatting makes Neovim set formatexpr, and vim.lsp.formatexpr() then
+-- silently no-ops on prose. vim.lsp.buf.format() is unaffected.
+vim.bo.formatexpr = ""
+vim.api.nvim_create_autocmd("LspAttach", {
+    buffer = 0,
+    callback = function(args)
+        vim.bo[args.buf].formatexpr = ""
+    end,
+})
